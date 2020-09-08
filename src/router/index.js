0350b3bf68877feb97d664870402daea5a6fa7f2
xuebapu <1972617324@qq.com>
2020-09-08 10:52:36 +0800

    登录页面

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+const Home =()=>import("views/home/Login");
+
+Vue.use(VueRouter)
+
+  const routes = [
+    {
+      path:'/',
+      redirect:'/home'
+    },
+    {
+      path: '/home',
+      component: Home,
+    },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+})
+
+export default router
